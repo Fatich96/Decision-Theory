82b95260525835b6d15c09f365d8ce32a2ff85a2
Fatich Osman <38991381+Fatich96@users.noreply.github.com>
2021-02-24 21:28:39 +0200

    Add files via upload

---
+++ pythonProject1/RNN.py
@@ -0,0 +1,168 @@
+#Libraries
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords,twitter_samples
+from nltk.tag import pos_tag
+from nltk.tokenize import TweetTokenizer,word_tokenize
+from nltk import FreqDist,classify,NaiveBayesClassifier
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.activations import *
+from keras import layers
+from keras.callbacks import ModelCheckpoint
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import re,string
+import numpy as np
+
+tt=TweetTokenizer()
+#Define modules for pre-process of data.
+#Remove noise removes URL ,Tweeter nicknames,punctuaries and stopwords 
+def remove_noise(tweet_tokens,stop_words=()):
+
+    cleaned_tokens=[]
+
+    for token,tag in pos_tag(tweet_tokens):
+        token=re.sub('http[s]?://(?:[a-zA-Z][0-9]|[$-_@.&+#]|[!*\(\),]|'\ 
+                     '(?:%[0-9a-fA-F][0-9a-fA-F]))+','',token) #removes URL
+        token=re.sub("(@[A-Za-z0-9_]+)","",token) #Removes the nicknames
+
+        print("token",token)
+
+        if tag.startswith("NN"):
+            pos='n'
+        elif tag.startswith("VB"):
+            pos='v'
+        else:
+            pos='a'
+
+        lmtzr=WordNetLemmatizer()
+        token=lmtzr.lemmatize(token,pos) #Lemmatizing the words 
+	
+	#removes punctuaries and stopwords
+        if len(token)>0 and token not in  string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+    return cleaned_tokens 
+
+
+
+
+
+
+#Run the main programme
+if __name__ == '__main__':
+
+    #Inputs the dataframes with pandas
+    positive_tweets =pd.read_csv("~/Desktop/DT-Datasets-part3/Project 7/SocialMedia_Positive.csv")
+    negative_tweets =pd.read_csv("~/Desktop/DT-Datasets-part3/Project 7/SocialMedia_Negative.csv")
+    #Tokenizing strings in the column "Text" of the dataframes 
+    positive_tweet_tokens= positive_tweets["Text"].apply(tt.tokenize)
+    negative_tweet_tokens = negative_tweets["Text"].apply(tt.tokenize)
+    
+    
+    #Merging vertically the dataframes under the name df
+    df= pd.concat([positive_tweets,negative_tweets])
+    
+    #Plot the amounts of data of the column "Sentiment" to check if they are biased
+    sns.set_theme()
+    sns.countplot(x='Sentiment',data=df)
+    plt.show()
+
+    #Import the list of the english stopwords
+    stop_words=stopwords.words("english")
+
+    #Creates two empty lists 
+    positive_cleaned_tokens_list = []
+    negative_cleaned_tokens_list = []
+    
+    #put the clean data of the in those lists
+    for tokens in positive_tweet_tokens:
+        positive_cleaned_tokens_list.append(remove_noise(tokens,stop_words))
+
+    for tokens in negative_tweet_tokens:
+        negative_cleaned_tokens_list.append(remove_noise(tokens,stop_words))
+
+
+
+
+
+    #Merging the two lists
+    dataset =  positive_cleaned_tokens_list + negative_cleaned_tokens_list
+    
+    #Count the length of the larger sentence in our dataset
+    max_ln = len(dataset[0])
+    for sentence in dataset:
+        print(sentence)
+        if max_ln < len(sentence):
+            max_ln = len(sentence)
+
+    #create a y variable with the list of the sentiments
+    y=df['Sentiment']
+    #transform "Positive" in 1's and "Negative" in 0's in the y list
+    y=np.array(list(map(lambda x: 1 if  x== 'positive' else 0,y)))
+    
+    #split Data for training and testing randomly
+    x_train,x_test,y_train,y_test = train_test_split(dataset,y, test_size=0.20,random_state=42)
+
+    
+    #Tokenizing data
+    tokenizer=Tokenizer(num_words=max_ln)
+    #Create a vocbulary that corresponds numbers in data
+    tokenizer.fit_on_texts(x_train)
+    x_train=tokenizer.texts_to_sequences(x_train)
+    x_test=tokenizer.texts_to_sequences(x_test)
+    print (x_train)
+    print(x_test)
+
+    #Takes the vocabulary size in order to create the matrix of the embending layer
+    vocab_size=len(tokenizer.word_index)+1
+    print(vocab_size)
+    
+    #The data should come as a vectors of the same size, we put 0 at the end of the shorter data
+    x_train=pad_sequences(x_train,padding='post',maxlen=max_ln)
+    x_test=pad_sequences(x_test,padding='post',maxlen=max_ln)
+    
+    #Create the model imput 3 Layers, one is LSTM so our NN is recursive
+    model1 = Sequential()
+    embedding_layer = layers.Embedding(vocab_size, max_words, input_length=max_ln, trainable=False)
+    model1.add(embedding_layer)
+    model1.add(layers.LSTM(128))
+    model1.add(layers.Dense(1, activation='sigmoid'))
+    model1.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
+    #Print Model's summary
+    print(model1.summary())
+
+    #Fiting and training the model in a 100 epochs, the NN inputs data in batches of 128(Input Layer)
+    history = model1.fit(x_train, y_train, batch_size=128, epochs=100, verbose=1, validation_split=0.2)
+
+    #Evaluate Model
+    score = model1.evaluate(x_test, y_test, verbose=1)
+    
+    #Print Accuracy and loss
+    print("Test Score:", score[0])
+    print("Test Accuracy:", score[1])
+    
+    #Plot an accuracy table
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    #Plot an error table
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
---
+++ pythonProject1/main_project.py
@@ -0,0 +1,117 @@
+#Libraries
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tag import pos_tag
+from nltk.tokenize import TweetTokenizer,word_tokenize
+from nltk import FreqDist,classify,NaiveBayesClassifier
+import pandas as pd
+import re,string,random
+
+tt=TweetTokenizer()
+#Define modules for pre-process of data.
+def remove_noise(tweet_tokens,stop_words=()):
+
+    cleaned_tokens=[]
+
+    for token,tag in pos_tag(tweet_tokens):
+        token=re.sub('http[s]?://(?:[a-zA-Z][0-9]|[$-_@.&+#]|[!*\(\),]|'\
+                     '(?:%[0-9a-fA-F][0-9a-fA-F]))+','',token)
+        token=re.sub("(@[A-Za-z0-9_]+)","",token)
+
+        #print("token",token)
+
+        if tag.startswith("NN"):
+            pos='n'
+        elif tag.startswith("VB"):
+            pos='v'
+        else:
+            pos='a'
+
+        lmtzr=WordNetLemmatizer()
+        token=lmtzr.lemmatize(token,pos)
+
+        if len(token)>0 and token not in  string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+    return cleaned_tokens
+
+def get_all_words(cleaned_tokens_list):
+    for tokens in cleaned_tokens_list:
+        for token in tokens:
+            yield token
+
+#Preparing data for input
+def get_tweets_for_model(cleaned_tokens_list):
+    for tweet_tokens in cleaned_tokens_list:
+        yield dict([token,True] for token in tweet_tokens)
+
+#Run the main programme
+if __name__ == '__main__':
+    
+    #Inputs the dataframes with pandas
+    positive_tweets =pd.read_csv("~/Desktop/DT-Datasets-part3/Project 7/SocialMedia_Positive.csv")
+    negative_tweets =pd.read_csv("~/Desktop/DT-Datasets-part3/Project 7/SocialMedia_Negative.csv")
+    #Tokenizing strings in the column "Text" of the dataframes 
+    positive_tweet_tokens= positive_tweets["Text"].apply(tt.tokenize)
+    negative_tweet_tokens = negative_tweets["Text"].apply(tt.tokenize)
+    
+    #Import the list of the english stopwords
+    stop_words=stopwords.words("english")
+
+    #Creates two empty lists 
+    positive_cleaned_tokens_list = []
+    negative_cleaned_tokens_list = []
+    
+    #put the clean data of the in those lists
+    for tokens in positive_tweet_tokens:
+        positive_cleaned_tokens_list.append(remove_noise(tokens,stop_words))
+
+    for tokens in negative_tweet_tokens:
+        negative_cleaned_tokens_list.append(remove_noise(tokens,stop_words))
+        
+    
+    #Call all the positive words
+    all_pos_words=get_all_words(positive_cleaned_tokens_list)
+    #Take the frequencies of every word in tweets datafram and print the 10 most common
+    freq_dist_pos=FreqDist(all_pos_words)
+
+    print("10 Most common words:",freq_dist_pos.most_common(10))
+    
+    #Prepare data for the input (create a list of dictionary sentences)
+    positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
+    negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
+    
+    #Create list that contains lists that contains our dictionary sentences and the string "possitive"
+    positive_dataset = [(tweet_dict, "Positive")
+                        for tweet_dict in positive_tokens_for_model]
+    #Create list that contains lists that contains our dictionary sentences and the string "negative"
+    negative_dataset = [(tweet_dict, "Negative")
+                        for tweet_dict in negative_tokens_for_model]
+    #Merging the list of data
+    dataset = positive_dataset + negative_dataset
+    #Randomize their position
+    random.shuffle(dataset)
+    #split dataset in 80% training and 20% as testing
+    value= 0.8*len(dataset)+1
+    train_dataset=dataset[:int(value)]
+    test_dataset=dataset[int(value):]
+
+
+    #Call and train Naives Bayes classifier
+    classifier = NaiveBayesClassifier.train(train_dataset)
+    #Check and print the accuracy with the testing data
+    print("Accuracy is:", classify.accuracy(classifier, test_dataset))
+    #Show the 10 more important words
+    print(classifier.show_most_informative_features(10))
+    #Create and run a testing tweet
+    custom_tweet = "I ordered just once from TerribleCo, they screwed up, never used the app again."
+    custom_tokens = remove_noise(word_tokenize(custom_tweet))
+    print(custom_tweet, classifier.classify(dict([token, True] for token in custom_tokens)))
+
+
+
+
+
+
+
+
+
